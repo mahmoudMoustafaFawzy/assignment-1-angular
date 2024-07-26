86986c8da9251049d7f1319ee665fb1deb7b1b3e
mahmoudMoustafaFawzy <mahmoudmoustafafawzy>
2024-07-26 18:33:16 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProtoflioComponent } from './protoflio/protoflio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent,ProtoflioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/protoflio/protoflio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protoflio',
+  standalone: true,
+  imports: [],
+  templateUrl: './protoflio.component.html',
+  styleUrl: '../home/home.component.css'
+})
+export class ProtoflioComponent {
+
+}
